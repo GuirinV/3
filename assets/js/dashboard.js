ad67d6ee90fd2fa5d5fb238233f70c13d432a613
GuirinV <guirinveronical@gmail.com>
2023-10-22 14:36:48 -0300

    Consumo de API: mostrado en una tabla

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -9,9 +9,51 @@ function cargarPacientes(){
 }
 
 function cargar(datos){
-    datos.forEach(element => {
-        const {id,name,username,email, address: {street, suite, city}}=element
-        //console.log(street)
-    });
+    // datos.forEach(element => {
+    //     const {id,name,username,email, address: {street, suite, city}}=element
+    //     //console.log(street)
+    // });
 
+    //Se crea la tabla
+    let html= `<table>
+                <thead>
+                <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Street</th>
+                <th>Suite</th>
+                <th>City</th>
+                </tr>
+                </thead>
+
+                <tbody>`
+    
+    //se agregan los datos a la tabla
+    html+=datos.reduce((ac,e)=>{
+        const {id,name,username,email,address: {street, suite, city}}=e
+        
+        ac+=`
+        <tr>
+        <td>${id}</td>
+        <td>${name}</td>
+        <td>${username}</td>
+        <td>${email}</td>
+        <td>${street}</td>
+        <td>${suite}</td>
+        <td>${city}</td>
+        </tr>
+        
+        `
+        return ac
+    },'')
+    html+=  `
+    </tbody>
+    
+    </table>`          
+    console.log (html)
+    const divdatos=document.querySelector('#datos')
+    divdatos.innerHTML=html
 }
+
